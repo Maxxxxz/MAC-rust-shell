70170f248c35b644d8b6433ea00ba7e5308889ca
Michael Cooper <mc454916@ohio.edu>
2020-08-18 00:10:40 -0400

    begin multiple options

--- src/shell.rs
+++ src/shell.rs
@@ -1,4 +1,6 @@
-use std::io;
+use std::io::{self, Write};
+
+// crashes when line is empty
 
 pub fn run() -> bool
 {
@@ -23,6 +25,8 @@ pub fn run() -> bool
 fn getMessage() -> String
 {
     print!("(\")> ");
+    io::stdout().flush().unwrap();
+
     let mut inp = String::new();
     io::stdin().read_line(&mut inp).expect("Failed to get input!");
     let mut msg = inp[..inp.len()-1].to_string();
@@ -38,4 +42,11 @@ fn getMessage() -> String
 fn parseMessage(msg: String)// -> Option? Something to tell the loop what went wrong
 {
     // split message by whitespace and parse via first command
+    let inVec: Vec<&str> = msg.split_whitespace().collect();
+
+    match inVec[0]
+    {
+        "help" => println!("this is help penguin <(\")"),
+        _ => println!("Unknown or invalid command."),
+    }
 }
