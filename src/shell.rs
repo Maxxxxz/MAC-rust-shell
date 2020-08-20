5b5e2a4d2f74c097d3a7cfa0819d47c223a234d4
Michael Cooper <mc454916@ohio.edu>
2020-08-19 22:01:35 -0400

    Add modular command system

--- src/shell.rs
+++ src/shell.rs
@@ -1,14 +1,25 @@
 use std::io::{self, Write};
 use std::collections::HashMap;
+use std::any::Any;
 
-// crashes when line is empty
+type inType = Vec<String>;
+type outType = bool;
+type shellType = fn(inType) -> outType;
 
 pub fn run() -> bool
 {
 
     // hashmap<str, fn()>
 
-    let cmd: HashMap<&str, fn() -> ()>;
+    // let mut cmd: HashMap<&str, &Any> = HashMap::new();
+    let mut cmd: HashMap<&str, shellType> = HashMap::new();
+
+    // builtin functions map
+    cmd.insert("", mac_und);
+    cmd.insert("ls", mac_ls);
+    cmd.insert("cd", mac_cd);
+    cmd.insert("help", mac_help);
+    cmd.insert("quit", mac_quit);
 
     let mut cmds: Vec<&str> = vec!["help", "quit"];
     // let user prefs = prefs
@@ -20,12 +31,36 @@ pub fn run() -> bool
     {
         let msg = getMessage();
 
-        if msg == "quit"
-        {
-            break;
-        }
+        // if msg == "quit"
+        // {
+        //     break;
+        // }
 
-        let _c = parseMessage(msg);
+        let mut _c: Vec<String> = parseMessage(msg);
+
+        // &_c[0] -> get first word in command (will be the function to run)
+        // [..] -> turn String into &str slice
+        // println!("{}", _c[0][..].to_string());
+
+        let func/*: fn(Vec<&str>) -> ()*/ =
+            cmd.get(&_c[0][..]);
+            // .unwrap();
+            // .downcast_ref::<shellType>();
+            // .unwrap();
+            // let func = func.downcast_ref::<shellType>().unwrap();
+        let continueLoop = match func
+        {
+            Some(f1) => f1(_c),
+            None => mac_unk(_c),
+        };
+
+        // let continueLoop = func(&mut _c);
+
+        match continueLoop
+        {
+            false => break,
+            _ => (),
+        }
 
     }
 
@@ -60,7 +95,19 @@ fn getMessage() -> String
 
 }
 
-fn parseMessage(msg: String)// -> Option? Something to tell the loop what went wrong
+fn parseMessage(msg: String) -> Vec<String>
+{
+    if msg.is_empty()
+    {
+        return vec!["".to_string()];
+    }
+    
+    let retVec: Vec<String> = msg.split_whitespace().map(|st| st.to_string()).collect();
+
+    return retVec;
+}
+
+fn _parseMessage(msg: String)// -> Option? Something to tell the loop what went wrong
 {
     // split message by whitespace and parse via first command
     if msg.is_empty()
@@ -70,9 +117,43 @@ fn parseMessage(msg: String)// -> Option? Something to tell the loop what went w
     
     let inVec: Vec<&str> = msg.split_whitespace().collect();
 
-    match inVec[0]
-    {
-        "help" => println!("this is help penguin <(\")"),
-        _ => println!("Unknown or invalid command."),
-    }
+    // match inVec[0]
+    // {
+    //     "help" => println!("this is help penguin <(\")"),
+    //     _ => println!("Unknown or invalid command."),
+    // }
+}
+
+fn mac_unk(cmds: Vec<String>) -> outType
+{
+    println!("Unknown command: {}", cmds[0]);
+    return true;
+}
+
+fn mac_und(cmds: Vec<String>) -> outType
+{
+    return true;
+}
+
+fn mac_ls(cmds: Vec<String>) -> outType
+{
+    println!("ls");
+    return true;
+}
+
+fn mac_cd(cmds: Vec<String>) -> outType
+{
+    println!("cd");
+    return true;
+}
+
+fn mac_help(cmds: Vec<String>) -> outType
+{
+    println!("this is help penguin <(\")");
+    return true;
+}
+
+fn mac_quit(cmds: Vec<String>) -> outType
+{
+    return false;
 }
